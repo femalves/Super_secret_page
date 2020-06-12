4066b395e8836f5c4b871a9ee1c79bba2e7e3204
Fernanda Alves <fernanda__m@hotmail.com>
2020-06-11 23:00:34 -0300

    redux setup, redux-logger, user on header'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Header from "./components/header/header";
 import Secret from "./pages/super-secret-page/super-secret-page";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   constructor() {
@@ -18,20 +23,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data(),
             },
           });
         });
-      } else {
-        this.setState({ currentUser: userAuth });
       }
+      setCurrentUser(userAuth);
     });
   }
 
@@ -45,12 +50,36 @@ class App extends React.Component {
       <div>
         <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path="/" component={SignInAndSignUp} />
-          <Route exact path="/secret" component={Secret} />
+          <Route
+            exact
+            path="/"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/secret" />
+              ) : (
+                <SignInAndSignUp />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/secret"
+            render={() =>
+              this.props.currentUser ? <Secret /> : <Redirect to="/" />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,12 +3,24 @@ import { Link } from "react-router-dom";
 
 import "./header.scss";
 import { auth } from "../../firebase/firebase.utils";
-
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
 const Header = ({ currentUser }) => (
   <div className="header">
-    <div className="option" onClick={() => auth.signOut()}>
-      <Link to="/">{currentUser ? <div>SIGN OUT</div> : ""}</Link>
+    <div className="options">
+      {currentUser ? (
+        <Link to="/" className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </Link>
+      ) : (
+        ""
+      )}
     </div>
   </div>
 );
-export default Header;
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+export default connect(mapStateToProps)(Header);
